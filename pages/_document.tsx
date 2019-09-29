c9d0831385185e7eddda982f0063404c4bfd9ca5
Piotr Sklepinski <Piotr.Sklepinski@stepstone.com>
2019-09-29 08:18:47 +0200

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import { ServerStyleSheet } from 'styled-components';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document<{styleTags: any}> {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const sheet = new ServerStyleSheet();
+
+    // Step 2: Retrieve styles from components in the page
+    const page = ctx.renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+    return { ...initialProps, ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+          {this.props.styleTags}
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class extends React.Component {
+  render() {
+    const StyledHeader = styled.div`
+      color: red;
+    `;
+    return (
+      <StyledHeader>Hello Next.js</StyledHeader>
+    )
+  }
+}
